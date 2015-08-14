d6748333134feca5633385e1a20cecf892f23c98
eminagov <pece.eminagov@gmail.com>
2015-08-15 01:49:32 +0200

    Added Generic API methods for testing purposes

---
+++ src/OnlineAPIBuilder/Classes/Helper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineAPIBuilder.Classes
+{
+    public static class Helper
+    {
+        public static Uri GetUri(HttpRequest request)
+        {
+            var builder = new UriBuilder();
+            builder.Scheme = request.Scheme;
+            builder.Host = request.Host.Value;
+            builder.Path = request.Path;
+            builder.Query = request.QueryString.ToUriComponent();
+            return builder.Uri;
+        }
+
+        public static string GetUriSegment(HttpRequest request, int segmentNo)
+        {
+            var segments = request.Path.ToUriComponent().Split('/');
+            return segments[segmentNo];
+        }
+    }
+}
---
+++ src/OnlineAPIBuilder/Controllers/GenericController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using OnlineAPIBuilder.Classes;
+
+namespace OnlineAPIBuilder.Controllers
+{
+    [Route("api/{client}/{version}/{callName}")]
+    public class ValuesController : Controller
+    {
+        // GET: api/{client}/{version}/{callName}/{*.}
+        [HttpGet("{*.}")]
+        public List<object> Get()
+        {
+            string client = Helper.GetUriSegment(Request, 2);
+            string version = Helper.GetUriSegment(Request, 3);
+            string callName = Helper.GetUriSegment(Request, 4);
+            var x = new object[] { "value1", "value2" };
+            return x.ToList();
+        }
+
+        // POST api/{client}/{version}/{callName}/{*.}
+        [HttpPost("{*.}")]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/{client}/{version}/{callName}/{*.}
+        [HttpPut("{*.}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/{client}/{version}/{callName}/{*.}
+        [HttpDelete("{*.}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
