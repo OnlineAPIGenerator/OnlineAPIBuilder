e7e3a8119b169286f3d1cdf9c806ac4a97741ed5
eminagov <pece.eminagov@gmail.com>
2015-08-15 15:05:59 +0200

    Added comments on the helper methods

--- src/OnlineAPIBuilder/Classes/Helper.cs
+++ src/OnlineAPIBuilder/Classes/Helper.cs
@@ -8,6 +8,11 @@ namespace OnlineAPIBuilder.Classes
 {
     public static class Helper
     {
+        /// <summary>
+        /// Not working: Needed for returning of the Request.RequestUri and its segments
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Generated Uri part of the Request</returns>
         public static Uri GetUri(HttpRequest request)
         {
             var builder = new UriBuilder();
@@ -18,6 +23,12 @@ namespace OnlineAPIBuilder.Classes
             return builder.Uri;
         }
 
+        /// <summary>
+        /// Parses segments from the Url path
+        /// </summary>
+        /// <param name="request">Basic HTTP Request</param>
+        /// <param name="segmentNo">The number of the url segment that you wish to be retrieved</param>
+        /// <returns>Segment of the url that you request</returns>
         public static string GetUriSegment(HttpRequest request, int segmentNo)
         {
             var segments = request.Path.ToUriComponent().Split('/');
